51d08f7a31e4baf1b1d4770745f6042ba6ea1f75
Oliver Trinidad <ocmtrinidad@gmail.com>
2024-04-03 11:31:52 +0800

    Created signupController

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const messages_Controller = require("../controllers/messagesController");
+const messages_controller = require("../controllers/messagesController");
+const signup_controller = require("../controllers/signupController");
 
-router.get("/", messages_Controller.index);
-router.get("/post", messages_Controller.create_post_get);
-router.post("/post", messages_Controller.create_post_post);
+router.get("/", messages_controller.index);
+router.get("/post", messages_controller.create_post_get);
+router.post("/post", messages_controller.create_post_post);
+
+router.get("/signup", signup_controller.signup_get);
+router.post("/signup", signup_controller.signup_post);
 
 module.exports = router;
--- routes/signup.js
+++ routes/signup.js
@@ -1,53 +1,53 @@
-const express = require("express");
-const router = express.Router();
-const asyncHandler = require("express-async-handler");
-const { body, validationResult } = require("express-validator");
-const User = require("../models/user");
-const bcrypt = require("bcryptjs");
+// const express = require("express");
+// const router = express.Router();
+// const asyncHandler = require("express-async-handler");
+// const { body, validationResult } = require("express-validator");
+// const User = require("../models/user");
+// const bcrypt = require("bcryptjs");
 
-router.get("/", (req, res, next) => {
-  res.render("signup", {
-    title: "Sign Up"
-  });
-});
+// router.get("/", (req, res, next) => {
+//   res.render("signup", {
+//     title: "Sign Up"
+//   });
+// });
 
-router.post("/", [
-  body("first_name", "First name must be specified").trim().isLength({ min: 1 }).escape(),
-  body("last_name", "Last name must be specified").trim().isLength({ min: 1 }).escape(),
-  body("username", "Username must be specified").trim().isLength({ min: 1 }).escape(),
-  body("password", "Password must be atleast 6 characters").trim().isLength({ min: 6 }).escape(),
-  body("confirmPassword", "Confirm password must match password")
-    .trim()
-    .custom((value, { req }) => {
-      return value === req.body.password;
-    }),
-  asyncHandler(async (req, res, next) => {
-    bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
-      if (err) {
-        return next(err);
-      } else {
-        const errors = validationResult(req);
-        const user = new User({
-          first_name: req.body.first_name,
-          last_name: req.body.last_name,
-          username: req.body.username,
-          password: hashedPassword,
-          member: false
-        });
-        if (!errors.isEmpty()) {
-          res.render("signup", {
-            title: "Sign Up",
-            user: user,
-            errors: errors.array()
-          });
-          return;
-        } else {
-          await user.save();
-          res.redirect("/login");
-        }
-      }
-    });
-  })
-]);
+// router.post("/", [
+//   body("first_name", "First name must be specified").trim().isLength({ min: 1 }).escape(),
+//   body("last_name", "Last name must be specified").trim().isLength({ min: 1 }).escape(),
+//   body("username", "Username must be specified").trim().isLength({ min: 1 }).escape(),
+//   body("password", "Password must be atleast 6 characters").trim().isLength({ min: 6 }).escape(),
+//   body("confirmPassword", "Confirm password must match password")
+//     .trim()
+//     .custom((value, { req }) => {
+//       return value === req.body.password;
+//     }),
+//   asyncHandler(async (req, res, next) => {
+//     bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+//       if (err) {
+//         return next(err);
+//       } else {
+//         const errors = validationResult(req);
+//         const user = new User({
+//           first_name: req.body.first_name,
+//           last_name: req.body.last_name,
+//           username: req.body.username,
+//           password: hashedPassword,
+//           member: false
+//         });
+//         if (!errors.isEmpty()) {
+//           res.render("signup", {
+//             title: "Sign Up",
+//             user: user,
+//             errors: errors.array()
+//           });
+//           return;
+//         } else {
+//           await user.save();
+//           res.redirect("/login");
+//         }
+//       }
+//     });
+//   })
+// ]);
 
-module.exports = router;
+// module.exports = router;
