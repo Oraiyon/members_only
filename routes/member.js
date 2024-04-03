a98ae160c8ebf578f4f7b0ac9a20d2d7861bbefa
Oliver Trinidad <ocmtrinidad@gmail.com>
2024-04-03 09:50:41 +0800

    Updated member page

--- routes/member.js
+++ routes/member.js
@@ -14,7 +14,11 @@ router.post(
   asyncHandler(async (req, res, next) => {
     const user = await User.findById(req.user.id).exec();
     if (req.body.passcode !== process.env.SECRET) {
-      res.render("member");
+      const messages = ["Turn back!", "You don't belong here...", "Wrong!", "Leave now..."];
+      const selectedMessage = messages[Math.floor(Math.random() * messages.length)];
+      res.render("member", {
+        message: selectedMessage
+      });
       return;
     } else {
       user.member = true;
