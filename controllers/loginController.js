cc90a1f103d91227024311642ed2016d796a79d5
Oliver Trinidad <ocmtrinidad@gmail.com>
2024-04-03 11:40:28 +0800

    Added logout middleware for "/logout"

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -10,3 +10,12 @@ exports.login_post = passport.authenticate("local", {
   successRedirect: "/",
   failureRedirect: "/signup"
 });
+
+exports.logout = (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+};
